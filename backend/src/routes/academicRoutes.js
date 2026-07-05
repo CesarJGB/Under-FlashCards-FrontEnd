d16a08de8864d2cf6a6021b7e5ff1c365e439c06
CesarJGB <97641767+CesarJGB@users.noreply.github.com>
2026-07-05 14:41:35 -0500

    Update academicRoutes.js

--- backend/src/routes/academicRoutes.js
+++ backend/src/routes/academicRoutes.js
@@ -7,16 +7,19 @@ const academicController = require('../controllers/academicController');
 // --- Endpoints de Materias ---
 router.get('/academic/materias/:userId', academicController.getMaterias);
 router.post('/academic/materias', academicController.createMateria);
+router.put('/academic/materias/:id', academicController.updateMateria);
 router.delete('/academic/materias/:id', academicController.deleteMateria);
 
 // --- Endpoints de Temas ---
 router.get('/academic/temas/:materiaId', academicController.getTemas);
 router.post('/academic/temas', academicController.createTema);
+router.put('/academic/temas/:id', academicController.updateTema);
 router.delete('/academic/temas/:id', academicController.deleteTema);
 
 // --- Endpoints de Subtemas ---
 router.get('/academic/subtemas/:temaId', academicController.getSubtemas);
 router.post('/academic/subtemas', academicController.createSubtema);
+router.put('/academic/subtemas/:id', academicController.updateSubtema);
 router.delete('/academic/subtemas/:id', academicController.deleteSubtema);
 
 // --- Endpoints de Parciales Activos ---
@@ -24,3 +27,4 @@ router.patch('/academic/materias/:id/active-parciales', academicController.updat
 router.get('/academic/materias/:id/domain-preview', academicController.getDomainPreview);
 
 module.exports = router;
+
