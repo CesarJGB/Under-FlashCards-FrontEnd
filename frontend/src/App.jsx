27211de58c1895c25d1cf5cf4d10ce9c2493cfe3
CesarJGB <97641767+CesarJGB@users.noreply.github.com>
2026-08-03 22:21:44 -0600

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -309,6 +309,7 @@ function DashboardScreen({ user, onLogout, onInviteRequired }) {
               pendingNav={pendingLibraryNav}
               clearPendingNav={() => setPendingLibraryNav(null)}
               dashboardShell={dashboardShell}
+              onCalendarImmersiveChange={handleCalendarImmersiveChange}
             />
           )}
 
